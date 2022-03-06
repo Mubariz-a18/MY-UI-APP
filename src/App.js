c2ee40b61bd5215bfa448d05ee7fccd4892cebcf
codeByMubariz <syedmubariz542@gmail.com>
2022-03-06 20:18:03 +0530

    counter component (state)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from './Header'
 import Footer from "./Footer";
 import Name from './Name.js'
-
+import Counter from "./utils/Counter";
 
 
 
@@ -11,8 +11,12 @@ import Name from './Name.js'
 function App() {
     return <div>
         <Header />
+        <Counter count={10}/>
+        <Counter count={100}/>
+
         <Name personName='mubariz' personAge='22'/>
         <Name personName='imran' personAge='23'/>
+        <img width="200" height="200" src="https://4.img-dpreview.com/files/p/E~TS1180x0~articles/3925134721/0266554465.jpeg" />
         <Footer />
     </div>
 }
